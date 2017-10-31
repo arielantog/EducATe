6c12425c4439ba8a3d7cf09a408491decff02603
arielantog <arielantog@gmail.com>
2017-10-30 21:19:15 -0300

    Crear el sistema al iniciar el server.

--- test/IniciarServer.java
+++ test/IniciarServer.java
@@ -1,9 +1,12 @@
 package test;
 
+import controlador.Sistema;
+
 public class IniciarServer {
 
 	public static void main(String[] args) {
 		try{
+			new Sistema();
 			rmi.Server.getInstancia().start();
 			System.out.println("Conectado");
 		}catch(Exception e){
